7acc7618847b750dcf26c5d37a658b44b38bf061
dhymasriyanto <gojin003@gmail.com>
2021-12-31 01:36:43 +0700

    add cors

--- templates/microservice-express-template/server.mjs
+++ templates/microservice-express-template/server.mjs
@@ -2,11 +2,14 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
-import { router } from './routes/index.mjs'
+import {router} from './routes/index.mjs'
+import cors from 'cors'
 
 const app = express()
 dotenv.config()
 const port = process.env.PORT
+
+app.use(cors())
 
 app.use(express.json()) // for parsing application/json
 
